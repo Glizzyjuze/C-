18a86dd1d36609c9b70410ac65a8e31e78bc9743
Hugo Zingmark <Hugo.priebezingmark2@gmail.com>
2022-08-30 10:27:34 +0200

    initial commit

---
+++ Dota3/Dota3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write your name bitch:");
+string name;
+name = Console.ReadLine();
+
+
+
+
+Console.WriteLine($"fuck you {name}");
+//Console.Writeline("Boomerang Monkey")
+
+Console.ReadLine();
